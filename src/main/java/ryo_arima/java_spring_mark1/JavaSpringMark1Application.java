3eef02966e286038b585b91289963abd3d39c9d6
Ryo Arima <ryo.arima@outlook.com>
2024-07-30 05:43:56 +0900

    2nd commit

---
+++ src/main/java/ryo_arima/java_spring_mark1/JavaSpringMark1Application.java
@@ -0,0 +1,13 @@
+package ryo_arima.java_spring_mark1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMark1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringMark1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ryo_arima/java_spring_mark1/JavaSpringMark1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ryo_arima.java_spring_mark1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringMark1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
