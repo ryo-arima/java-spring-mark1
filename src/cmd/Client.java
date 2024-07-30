fe0f32fd57344ffe1c9b5645c378390f1d85380e
Ryo Arima <ryo.arima@outlook.com>
2024-07-31 01:18:15 +0900

    init project tree

--- src/cmd/Client.java
+++ src/cmd/Client.java
@@ -1,10 +1,10 @@
-package ryo_arima.java_spring_mark1;
+package ryo_arima.java_spring_mark1.cmd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaSpringMark1Application {
+public class Client {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringMark1Application.class, args);
---
+++ src/cmd/Server.java
@@ -0,0 +1,13 @@
+package ryo_arima.java_spring_mark1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringMark1Application.class, args);
+	}
+
+}
